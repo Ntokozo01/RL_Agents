14bc16f9b0ce8c900579152ffbe6c8c65eddb8c1
Mduduzi <ndlmdu011@myuct.ac.za>
2024-05-08 11:55:33 +0200

    Added code to find the packages by try and error without any learning

--- Scenario1.py
+++ Scenario1.py
@@ -1,14 +1,52 @@
 from FourRooms import FourRooms
 import numpy as np
+import random
 
 NUM_ROWS = 13
 NUM_COLS = 13
 
-observed_env = np.zeros((NUM_COLS, NUM_ROWS))
+observed_environment = np.zeros((NUM_COLS, NUM_ROWS))
 
 MOVES = [FourRooms.UP, FourRooms.DOWN, FourRooms.LEFT, FourRooms.RIGHT]
 
-FourRooms_obj = FourRooms("simple")
-print("Agent starts at: {0}".format(FourRooms_obj.getPosition()))
+def findPackage(i, fourRoomsObj):
+    for j in range(num_iterations):
+        action = MOVES[random.randint(0, 3)]
+        
+        
+        gridType, (nextPosX, nextPosY), packagesRemaining, isTerminal = fourRoomsObj.takeAction(action)
+    
+        print("Agent took {0} action and moved to {1}".format (action, (nextPosX, nextPosY)))
+        
+        if packagesRemaining == 0:
+            print("Package collected")
+            fourRoomsObj.showPath(-1, "image_{0}.png".format(i))
+            return j
+    
+        if isTerminal:
+            print("Reached Terminal")
+            fourRoomsObj.showPath(-1, "image_{0}.png".format(i))
+            return j
 
-FourRooms_obj.showPath(-1, "image_0.png")
+fourRoomsObj = FourRooms("simple")
+
+NUM_EPOCHS = 10
+
+k = fourRoomsObj.getPackagesRemaining()
+print("Packages to collect:", k)
+
+(startX, startY) = fourRoomsObj.getPosition() 
+print("Agent starts at: {0}".format((startX, startY)))
+
+for i in range(NUM_EPOCHS):
+    fourRoomsObj.newEpoch()
+    
+    num_iterations = 1000
+    total_actions = findPackage(i, fourRoomsObj)
+    
+    print("Total actions taken:", total_actions)
+    
+    
+     
+
+fourRoomsObj.showPath(-1)
